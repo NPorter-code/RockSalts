f7e3a3ce84b03af2c189b462f12bfec28d148441
NPorter-code <nathanielgaryporter@gmail.com>
2025-10-23 09:48:35 -0600

    Set up the basic php/sql for the contact (needing later revision)

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Trying to get php to work</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
 <?php
 
-// define variables and set to empty values
-$nameErr = $emailErr = $commentErr = "";
-$name = $email = $comment = "";
+// define variables
+$name = $_POST["name"];
+$email = $_POST["email"];
+$comment = $_POST["comment"];
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Name is Required"
-    } else {
-        $name = $_POST["name"]
-    }
-    if (empty($_POST["email"]))
+
+//Log into server
+$servername = "localhost";
+$username = "ChaosCat";
+$password = "WhiteB1ackTe@";
+
+// Create Connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check Connection
+if ($conn->connect_error) {
+    die("Connection Failed: ". $conn->connect_error);
+} else{
+    echo "Connected Successfully <br><br>";
 }
 
+if ($Name === "" and $comment === "" and $email === "") { # if sName and author === nothing dont put into database
+    echo "All Feilds Required"; 
+} else { # put data into database
+$sql = "INSERT INTO saltData (dispName, Email, Comment);
+VALUES ('$Name', '$email', '$comment')";
+}
 
+if ($conn->query($sql) === TRUE) {
+    echo "This fuckin shit worked, HELL YEAH!!";
+} else {
+    echo "Damn it, it didn't work, you fucked it up man...  :( <br><br> Error:" . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT fileID, dispName, Email, Comment FROM saltData";
+$result = $conn->query($squl);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["dispName"]. " - Email: " . $row["Email"]. "- Comment:" . $row["Comment"]. "<br>";
+    }
+    else {
+        echo "0 results";
+    }
+}
+
+$conn->close();
 ?>
+
+</body>
+</html>
