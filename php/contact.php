11cae8d99bbe193845d0ff96ea80db4ea6cfb05c
NPorter-code <nathanielgaryporter@gmail.com>
2025-10-25 13:18:00 -0600

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -51,10 +51,10 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "id: " . $row["id"]. " - Name: " . $row["dispName"]. " - Email: " . $row["Email"]. "- Comment:" . $row["Comment"]. "<br>";
     }
+}
     else {
         echo "0 results";
     }
-}
 
 $conn->close();
 ?>
