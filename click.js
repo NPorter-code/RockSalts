f313a130fa1497b3aa05348de19f17b89653c9a0
NPorter-code <nathanielgaryporter@gmail.com>
2025-12-09 10:10:38 -0700

    A lot of system and mobile view things

---
+++ click.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    document.getElementById("dropdown").classList.toggle("show")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('dropdown')) {
+        var dropdowns = this.document.getElementsByClassName("btn");
+        var i;
+        for (i = 0; i< dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
