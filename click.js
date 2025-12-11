726fbb3857a972647d50694e542d3e5aa82ad9af
NPorter-code <nathanielgaryporter@gmail.com>
2025-12-11 09:01:23 -0700

    maintinence

--- click.js
+++ click.js
@@ -1,10 +1,10 @@
 function myFunction(){
-    document.getElementById("dropdown").classList.toggle("show")
+    document.getElementById("myDropdown").classList.toggle("show")
 }
 
 window.onclick = function(e) {
     if (!e.target.matches('dropbtn')) {
-        var dropdown = document.getElementsById("dropdown");
+        var dropdown = document.getElementsById("myDropdown");
         if (dropdown.classList.contains('show')) {
             dropdown.classList.remove('show');
         }
