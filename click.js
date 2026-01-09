6d69281c79597a4df8c5c554c55e269cb1c9b004
NPorter-code <nathanielgaryporter@gmail.com>
2026-01-09 10:04:44 -0700

    some shitty coding designed to try to fix dropdown problems

--- click.js
+++ click.js
@@ -3,7 +3,7 @@ function myFunction(){
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('dropbtn')) {
+    if (!event.target.matches('.dropbtn')) {
         var dropdown = document.getElementsByClassName("dropdown-content");
         if (dropdown.classList.contains('show')) {
             dropdown.classList.remove('show');
